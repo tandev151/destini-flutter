ba163f20adcbbff7052d28efb9bd8f82b06a5823
Tan IT <tandev151>
2024-03-13 06:37:25 +0700

    done app

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -88,6 +88,13 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    if ([0, 1, 2].contains(_storyNumber)) {
+      return true;
+    }
+    return false;
+  }
 }
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
